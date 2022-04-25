021f9006d9e0b13a2c5dd4f54993065b5bdfdcfd
xzyra14 <83936947+xzyra14@users.noreply.github.com>
2022-04-26 02:42:18 +0800

    Add files via upload

---
+++ Activity2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity2 : ContentPage
+    {
+        public Activity2()
+        {
+            InitializeComponent();
+        }
+
+        void OnSlideValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valuelabel.Text = args.NewValue.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button) sender;
+            await DisplayAlert("Clicked!", "The button labeled '" + button.Text + "' has been clicked", "OK");
+        }
+    }
+}
